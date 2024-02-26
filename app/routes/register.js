369778313f9317ba845ccf81db60c62059e97c13
Jesreel Amorganda <117733007+IamRegexxt@users.noreply.github.com>
2024-02-27 00:39:33 +0800

    binalik ko sa dati, nag test lang

--- app/routes/register.js
+++ app/routes/register.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const db = require('../config/db.config');
-const { promisify } = require('util'); // Import promisify from util
 const router = express.Router();
 
-// Promisify the execute function
-const executeQuery = promisify(db.execute).bind(db);
-
 router.post('/register', async (req, res) => {
-    try {
+    try{
         const {name, email, username, password} = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hashSync(password, 10);
 
         const insertUserQuery = 'INSERT INTO users (name, email, username, password) VALUES (?, ?, ?, ?)';
-        await executeQuery(insertUserQuery, [name, email, username, hashedPassword]);
+        await db.promise().execute(insertUserQuery, [name, email, username, hashedPassword]);
 
         res.status(201).json({message: 'User created successfully'});
     } catch (error) {
