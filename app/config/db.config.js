2395bfced14b63247bef4ec7bf570219edb59586
Jesreel Amorganda <117733007+IamRegexxt@users.noreply.github.com>
2024-03-03 04:55:47 +0800

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,17 +1,24 @@
 const mysql = require('mysql2');
-const db = mysql.createConnection({
-    host: '34.124.178.40',
-    user: 'task-manager24',
-    password: 'bscs2a',
-    database: 'csfe3a'
+
+// Create a connection pool
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: '34.124.178.40',
+  user: 'task-manager24',
+  password: 'bscs2a',
+  database: 'csfe3a'
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err);
-    } else {
-        console.log('Connected to MySQL successfully');
-    }
+// Get a connection from the pool
+const db = pool.promise();
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL successfully');
+  connection.release(); // Release the connection back to the pool
 });
 
 module.exports = db;
