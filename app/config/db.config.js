8ae61ab5df30ab94c7cb67fdba01d46c56f7c8fc
Killiana <132580601+m-a-cruz@users.noreply.github.com>
2024-03-25 13:23:44 +0800

    db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,17 +1,26 @@
 const mysql = require('mysql2');
-const db = mysql.createConnection({
+
+const db = mysql.createPool({
+    connectionLimit: 10, // Maximum number of connections in the pool
     host: '193.203.166.156',
     user: 'u672161143_bwisit',
     password: 'Bwisit1.',
     database: 'u672161143_bwisit'
-});
+  });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err);
-    } else {
-        console.log('Connected to MySQL successfully');
-    }
-});
+// const db = mysql.createConnection({
+//     host: '193.203.166.156',
+//     user: 'u672161143_bwisit',
+//     password: 'Bwisit1.',
+//     database: 'u672161143_bwisit'
+// });
+
+// db.connect((err) => {
+//     if (err) {
+//         console.error('Error connecting to MySQL:', err);
+//     } else {
+//         console.log('Connected to MySQL successfully');
+//     }
+// });
 
 module.exports = db;
