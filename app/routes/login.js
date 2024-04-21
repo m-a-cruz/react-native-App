08eaf0c5694138be97b61d9396f7b4188edc83c9
Killiana <132580601+m-a-cruz@users.noreply.github.com>
2024-04-21 13:39:30 +0800

    login.js

--- app/routes/login.js
+++ app/routes/login.js
@@ -21,7 +21,7 @@ router.post('/login', async (req, res) => {
             return res.status(401).json({message: 'Invalid credentials'});
         }
         const token = jwt.sign({userId: user.id, username:user.username}, secretKey,{expiresIn:`10h`});
-        res.status(200).json({message: 'Login successful', token});
+        res.status(200).json({message: 'Login successful', user_id: user.id, token});
 
     } catch (error) {
         console.error('Error logging in:', error);
